a6573d2942ad06d237cb7e275edc56ec1fa8072f
John Quinn <johnjquinn01@outlook.com>
2022-10-07 15:20:38 -0400

    replace with user tokens in server/config.js

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    consumer_token: "<INSERT CONSUMER TOKEN HERE>",
+    consumer_secret: "<INSERT CONSUMER SECRET HERE>",
+    access_token_key: "<INSERT ACCESS TOKEN HERE>",
+    access_token_secret: "<INSERT ACCESS TOKEN SECRET HERE>",
+    bearer_token: "<INSERT BEARER TOKEN HERE>",
+    client_id: "<INSERT CLIENT ID HERE>",
+    client_secret: "<INSERT CLIENT SECRET HERE>"
+}
