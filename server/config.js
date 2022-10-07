5a56795b0951e25891d9b33171ba82a80d50534f
John Quinn <johnjquinn01@outlook.com>
2022-10-07 15:09:45 -0400

    clearing out

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    consumer_key: 'lGqY7NULvMpmlf7EvxPPYc8Sh',
-    consumer_secret: 'MvTcTtEE5zYFacABFff2a4NsXfdiCUIIV02YsK4k0AJ4aS5Xps',
-    access_token_key: '1221983238358552581-dRGjHGJSlxsjeTmyGV7WDJzbZfNJUb',
-    access_token_secret: 'g5fex4auvyqmSONXFDhKtoi2qEGrvez88BXrddk2kdxoZ',
-    bearer_token: "AAAAAAAAAAAAAAAAAAAAAPUYZgEAAAAApSGalRy3T9nKP4n%2FK4jXQ5GsZFE%3DXmOO7sFmMv4ntX2xWyMaRVFHPHHapmuXvFG5zxBtp06Rx9j8i6",
-    client_id: "MEVxS3NGRnV6bkxNWDJsXzVmM3c6MTpjaQ",
-    client_secret: "PRQYekovh9IX9S34WwaNnzgKbbgbw1_mT7254eNcTGtgOxJV1u"
-}
