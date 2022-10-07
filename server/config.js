0be949667f2fb79e1f0cddae6596422d6ba4aad8
cpt-friday <80221282+cpt-friday@users.noreply.github.com>
2022-10-07 15:05:14 -0400

    detokened config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    consumer_key: 'lGqY7NULvMpmlf7EvxPPYc8Sh',
-    consumer_secret: 'MvTcTtEE5zYFacABFff2a4NsXfdiCUIIV02YsK4k0AJ4aS5Xps',
-    access_token_key: '1221983238358552581-dRGjHGJSlxsjeTmyGV7WDJzbZfNJUb',
-    access_token_secret: 'g5fex4auvyqmSONXFDhKtoi2qEGrvez88BXrddk2kdxoZ',
-    bearer_token: "AAAAAAAAAAAAAAAAAAAAAPUYZgEAAAAApSGalRy3T9nKP4n%2FK4jXQ5GsZFE%3DXmOO7sFmMv4ntX2xWyMaRVFHPHHapmuXvFG5zxBtp06Rx9j8i6",
-    client_id: "MEVxS3NGRnV6bkxNWDJsXzVmM3c6MTpjaQ",
-    client_secret: "PRQYekovh9IX9S34WwaNnzgKbbgbw1_mT7254eNcTGtgOxJV1u"
+    consumer_token: "<INSERT CONSUMER TOKEN HERE>",
+    consumer_secret: "<INSERT CONSUMER SECRET HERE>",
+    access_token_key: "<INSERT ACCESS TOKEN HERE>",
+    access_token_secret: "<INSERT ACCESS TOKEN SECRET HERE>",
+    bearer_token: "<INSERT BEARER TOKEN HERE>",
+    client_id: "<INSERT CLIENT ID HERE>",
+    client_secret: "<INSERT CLIENT SECRET HERE>"
 }
